5a12013ab3e8b81f373333151103debb6a216b60
matvei <berezovskiymatvey@gmail.com>
2026-09-22 15:42:02 +0000

    удалили каталоги и файлы

---
+++ script4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd lab0
+
+rm arcobaleno/kitchen/katya_recipe
+rm arcobaleno/office/chef_recipe
+rm rival_kitchen
+rm critic_visit
+rm claude_monet/tasting_room/critic_copy
+rm arcobaleno/archive/review_archive
+rmdir arcobaleno/archive
+rm -r claude_monet/chef_office/reviews_copy
